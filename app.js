ba6c70907a619745361e24db2d77d46b8aa51bcc
doveriko <abelmorpe@gmail.com>
2020-04-15 17:48:27 +0200

    Structure with Irongenerate and basic auth set up on app.js

---
+++ app.js
@@ -0,0 +1,87 @@
+// Load .env values
+require("dotenv").config();
+
+const express = require("express");
+const mongoose = require("mongoose");
+const hbs = require("hbs");
+const cookieParser = require("cookie-parser");
+const path = require("path");
+const logger = require("morgan");
+const createError = require("http-errors");
+// const bodyParser   = require('body-parser');
+
+// Packages for authentication: 
+const session = require("express-session");
+const MongoStore = require("connect-mongo")(session);
+
+const app = express();
+
+const dbName = "tinder-dogs";
+mongoose.connect(`mongodb://localhost:27017/${dbName}`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  // .then((x) => {
+  //   console.log(
+  //     `Connected to Mongo! Database name: "${x.connections[0].name}"`
+  //   );
+  // })
+  // .catch((err) => {
+  //   console.error("Error connecting to mongo", err);
+  // });
+
+// const app_name = require("./package.json").name;
+// const debug = require("debug")(
+//   `${app_name}:${path.basename(__filename).split(".")[0]}`
+// );
+
+// View engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "hbs");
+
+// Middleware Setup
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+// Session middleware
+// The session package creates a new session middleware for authentication
+app.use(
+  session({
+    secret: "basic-auth-secret", // Used to sign the session ID cookie
+    cookie: { maxAge: 3600000 * 1 },	// expiration date of the cookie (1 day, in milliseconds)
+    resave: true,
+    saveUninitialized: false,
+    store: new MongoStore({ // Creates a new instance of connect-mongo to store the session information in our Mongo DB
+      mongooseConnection: mongoose.connection,
+      ttl: 60 * 60 * 24 * 7 // Time to live - 7 days (14 days - Default)
+    })
+  })
+);
+
+// default value for title local
+app.locals.title = 'DAWG - A social network for your dog';
+
+// Routes
+app.use('/', require('./routes/index'));
+// app.use('/', require('./routes/site-routes'));
+
+// Catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// Error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
+});
+
+module.exports = app;
