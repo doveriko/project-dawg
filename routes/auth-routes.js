7f00451f48a12753a6c46ff1afd3545469514dc7
doveriko <abelmorpe@gmail.com>
2020-04-16 17:02:40 +0200

    auth routes pre-set

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -12,4 +12,11 @@ router.get('/', (req, res, next) => {
   res.render('index');
 });
 
+router.get("/logout", (req, res, next) => {
+  req.session.destroy((err) => {
+    // cannot access session here
+    res.redirect("/login");
+  });
+});
+
 module.exports = router;
