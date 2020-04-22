1cab364a4fa261af62244502591c65cbff304529
doveriko <abelmorpe@gmail.com>
2020-04-22 18:03:34 +0200

    update cloudinary configuration

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -6,9 +6,9 @@ require('dotenv').config();
 
  
 cloudinary.config({ 
-    cloud_name: "doveriko", 
-    api_key: "894655459995122", 
-    api_secret: "JwsSufznjqpz9_OgRtQMm13E6wU"
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+    api_key: process.env.CLOUDINARY_API_KEY, 
+    api_secret: process.env.CLOUDINARY_API_SECRET 
   });
 
 const storage = cloudinaryStorage({
