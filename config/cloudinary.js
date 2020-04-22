ffc50d299a5c7378bf80941766f0e75b52345659
doveriko <abelmorpe@gmail.com>
2020-04-22 17:20:38 +0200

    testing cloudinary config

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -1,20 +1,22 @@
-const cloudinary = require("cloudinary");
-const cloudinaryStorage = require("multer-storage-cloudinary");
-const multer = require("multer");
-require("dotenv").config();
+const cloudinary = require('cloudinary');
+const cloudinaryStorage = require('multer-storage-cloudinary');
+const multer = require('multer');
 
-cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET
-});
+require('dotenv').config();
+
+ 
+cloudinary.config({ 
+    cloud_name: "doveriko", 
+    api_key: process.env.CLOUDINARY_API_KEY, 
+    api_secret: process.env.CLOUDINARY_API_SECRET 
+  });
 
 const storage = cloudinaryStorage({
   cloudinary: cloudinary,
-  folder: "dawg",
-  allowedFormats: ["jpg", "png", "gif"],
+  folder: 'dawg',
+  allowedFormats: ['jpg', 'png']
 });
-
+ 
 const parser = multer({ storage: storage });
 
-module.exports = parser;
+module.exports = parser
