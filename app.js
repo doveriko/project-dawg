abc5188b005e148bf42fc87b7a75bc1033638b57
doveriko <abelmorpe@gmail.com>
2020-04-22 17:05:30 +0200

    remove failing mongoose config in app.js

--- app.js
+++ app.js
@@ -24,14 +24,6 @@ mongoose
   .connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-  })
-  .then((x) => {
-    console.log(
-      `Connected to Mongo! Database name: "${x.connections[0].name}"`
-    );
-  })
-  .catch((err) => {
-    console.error("Error connecting to mongo", err);
   });
 
 // View engine setup
